c7196fb3672db740a13d1ef9cf75db3c590bfd80
suraj-projects <surajpadalkar171299.com>
2024-11-27 19:51:54 +0530

    Implemented winston and morgan logger

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import express from 'express'
 import 'dotenv/config'
+import logger from './logger.js'
+import morgan from 'morgan'
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
+
 let teaData = []
 let nextId = 1
 
 app.post('/teas', (req, res) => {
-    const {name, price} = req.body
+    const { name, price } = req.body
     const newTea = {
         id: nextId++,
         name,
@@ -26,20 +46,20 @@ app.get('/teas', (req, res) => {
 
 app.get('/tea/:id', (req, res) => {
     const tea = teaData.find(t => t.id === parseInt(req.params.id))
-    if(!tea){
+    if (!tea) {
         res.status(404).send('Tea not found')
-    }else{
+    } else {
         res.status(200).send(tea)
     }
 })
 
 app.put('/tea/:id', (req, res) => {
     const tea = teaData.find(t => t.id === parseInt(req.params.id))
-    if(!tea){
+    if (!tea) {
         res.status(404).send('Tea not found')
     }
 
-    const {name, price} = req.body
+    const { name, price } = req.body
     tea.name = name
     tea.price = price
     res.send(200).send(tea)
@@ -48,7 +68,7 @@ app.put('/tea/:id', (req, res) => {
 app.delete('/tea/:id', (req, res) => {
     const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
 
-    if(index === -1){
+    if (index === -1) {
         return res.status(404).send('Tea not found')
     }
 
